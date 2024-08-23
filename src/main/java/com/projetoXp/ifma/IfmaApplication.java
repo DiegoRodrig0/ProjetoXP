4ac3d5e2bfbc584e1ffd0025aa70454b8c36bad0
DiegoRodrig0 <rodrigoteixeira06@hotmail.com>
2024-08-22 23:48:50 -0300

    Primeiro commit - configs iniciais

---
+++ src/main/java/com/projetoXp/ifma/IfmaApplication.java
@@ -0,0 +1,13 @@
+package com.projetoXp.ifma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmaApplication.class, args);
+	}
+
+}
